27b01cf45e36fbf531bb616e868241a4e99d96b1
RD_dev <TzJBKTvQ7cTwuu8T>
2019-04-13 23:55:56 +0200

    -- test are working

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StoresModule } from './stores/stores.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(),
+        StoresModule,
+      ],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -15,8 +21,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should contain welcome message', () => {
+      expect(appController.getHello()).toContain('Welcome to Starbucks stores location API');
     });
   });
 });
--- src/stores/stores.controller.spec.ts
+++ src/stores/stores.controller.spec.ts
@@ -1,11 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StoresController } from './stores.controller';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Store } from './entities/store.entity';
+import { StoresService } from './stores.service';
 
 describe('Stores Controller', () => {
   let controller: StoresController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        StoresService,
+        {
+          provide: getRepositoryToken(Store),
+          useValue: {},
+        },
+      ],
       controllers: [StoresController],
     }).compile();
 

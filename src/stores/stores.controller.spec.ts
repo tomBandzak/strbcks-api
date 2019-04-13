d85b00b3bb2fd8dac4f2308a9e917464e2b2531a
RD_dev <TzJBKTvQ7cTwuu8T>
2019-04-13 16:18:57 +0200

    -- initial stores module

---
+++ src/stores/stores.controller.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { StoresController } from './stores.controller';
+
+describe('Stores Controller', () => {
+  let controller: StoresController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [StoresController],
+    }).compile();
+
+    controller = module.get<StoresController>(StoresController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});

3a6f4dc2ec295d722a6180c87d78e2d87b13b924
RD_dev <TzJBKTvQ7cTwuu8T>
2019-04-13 17:22:11 +0200

    -- root endpoint modified

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '' +
+      '<h1>Welcome to Starbucks stores location API</h1>' +
+      '<h2>In urgent need of coffee read instructions above</h2>' +
+      '<p>For unfiltered list of SB stores use /stores endpoint</p>';
   }
 }
